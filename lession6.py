4cc3610c8605042905240e98c332ad1db5a5a3f2
Nguyen Duc Hieu <duchieu97.hn@gmail.com>
2025-01-21 20:58:40 +0700

    commit bài hát yêu thích

--- lession6.py
+++ lession6.py
@@ -6,51 +6,22 @@ hieu: https://ict-mhieu-python-4.streamlit.app/
 
 
 import streamlit as st
-import time
 
-st.set_page_config(page_title="Thổi bóng bay", page_icon=":smile:", layout="wide")
-
-button = st.button("Thổi bóng bay lên")
-
-if button:
-    progress_bar = st.progress(0)
-    st.write("Đang xử lý dữ liệu bóng bay . . . . . .")
-
-
-    percent_complete = 0
-    for percent in range(100):
-        time.sleep(0.1)
-        percent_complete += percent + 10
-        if percent_complete > 100:
-            percent_complete = 100
-        progress_bar.progress(percent_complete)
-        if percent_complete == 100:
-            st.balloons()
-            break
-
-with st.expander("Thực hành"):
-    st.write("Đây là thông tin mở rộng")
-    
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("Đây là cột dành cho mèo")
-    st.write("Con mèo")
-
-with col2:
-    st.header("Đây là cột dành cho hổ")
-    st.write("Con hổ")
-    
-with col3:
-    st.header("Đây là cột dành cho rồng")
-    st.write("Con rồng")
-    
-st.sidebar.write("1. Lời nói đầu")
+st.set_page_config(page_title="Bài hát yêu thích", page_icon=":smile:", layout="wide")
 
 with st.sidebar:
-    st.sidebar.write("2. Mục lục")
+    img_path = "https://vcdn1-giaitri.vnecdn.net/2019/07/03/sontungmtp-1562144392-2006-1562144411.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=KA3cbHW_fed0Z-ErY8pEmA"
+    st.image(img_path, caption="Sơn Tùng M-TP")
+    st.write("Họ và tên: Nguyễn Thanh Tùng")
+    st.write("Nghệ danh: Sơn Tùng M-TP")
+    st.write("Nguyễn Thanh Tùng, thường được biết đến với nghệ danh Sơn Tùng M-TP, là một nam ca sĩ kiêm sáng tác nhạc, nhà sản xuất thu âm, rapper và diễn viên người Việt Nam. Nổi tiếng vì tầm ảnh hưởng sâu rộng đối với âm nhạc Việt Nam, anh được mệnh danh là 'Hoàng tử V-pop' bởi Giải thưởng Âm nhạc Thế giới và BroadwayWorld.")
 
-image_path = "https://i1-vnexpress.vnecdn.net/2025/01/21/huy-6161-1737434813-3768-1737435120.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=NYdZpvG8-K_W6p5gg0RMww"
-st.image(image_path, caption="Ảnh tên lửa")
 
-st.audio("sample-12s.mp3", format="audio/mp3")
+st.title("Bài hát yêu thích")
+st.write("Đừng làm trái tim anh đau - Sơn Tùng MTP")
+
+st.audio("dlttad.mp3", format="audio/mp3")
+
+st.title("MV yêu thích")
+
+st.video("https://www.youtube.com/watch?v=abPmZCZZrFA", format="video/mp4")
